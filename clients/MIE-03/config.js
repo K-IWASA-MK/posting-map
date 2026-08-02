15e599e77df63d13b13fb1159e41b359dd736a31
K-IWASA-MK <205480127+K-IWASA-MK@users.noreply.github.com>
2026-08-02 11:59:13 +0900

    fix(config): Correct spreadsheetId in MIE-03 config and deployment.json

--- clients/MIE-03/config.js
+++ clients/MIE-03/config.js
@@ -8,7 +8,8 @@ window.PMS_CLIENT_CONFIG = {
   districtId: "MIE-03",
   districtName: "三重県第3区",
   status: "ACTIVE_DEVELOPMENT",
-  spreadsheetId: "17VISNdxQLpxkR18XR4AMXRwDBSa600AJFIwrqDriQYxo8Tsot2DvXAzX",
+  
+  spreadsheetId: "1xQUvlCaUO103rjSGmdcFQQFkukodG4Dg9mS_teWT7uA",
   environment: "production",
   api: {
     gasWebAppUrl: "https://script.google.com/macros/s/AKfycbyjNwgZ_6CCv258lqKMrCXJYi0wDR23ZCyyzOQIV1R_WcCF5TQxYXOzZWWSJd_vMyu_/exec"
