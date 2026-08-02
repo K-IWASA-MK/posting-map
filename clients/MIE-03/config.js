5754225d1a01c03156566317e15e508e0c102fb9
K-IWASA-MK <205480127+K-IWASA-MK@users.noreply.github.com>
2026-08-02 14:26:49 +0900

    fix(config): sync MIE-03 GAS deployment endpoint to @59/@60

--- clients/MIE-03/config.js
+++ clients/MIE-03/config.js
@@ -12,7 +12,7 @@ window.PMS_CLIENT_CONFIG = {
   spreadsheetId: "1xQUvlCaUO103rjSGmdcFQQFkukodG4Dg9mS_teWT7uA",
   environment: "production",
   api: {
-    gasWebAppUrl: "https://script.google.com/macros/s/AKfycbwIgW7T_MjtiWF2nZCyWKivYGC4xH_x68X4tzBMsv-2Pod1rNa51E9HCQmencuaS2hd/exec"
+    gasWebAppUrl: "https://script.google.com/macros/s/AKfycbzmhVJ-En6DvZROcHZth5GbflgU0t8oMhlWdCKQ0sNg9qWpbpr3stbt2gEgApbuGoop/exec"
   },
   line: {
     liffId: "2010374196-bHBYo37e"
